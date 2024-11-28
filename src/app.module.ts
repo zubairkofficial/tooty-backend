87a80796c7ece386f32841264205b3901b4a8a5d
zubairkofficial <mzubairkhan.official@gmail.com>
2024-11-28 05:18:27 -0800

    set variables regrading production env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,15 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './user/entities/user.entity';
 import { Otp } from './user/entities/otp.entity';
-import dbConfig_DEVELOPMENT from './config/config.development';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
-      expandVariables: true,
-      load: [dbConfig_DEVELOPMENT],
+      isGlobal: true,      
     }),
     SequelizeModule.forRoot({
       dialect: "postgres",
+      host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
@@ -25,7 +22,14 @@ import dbConfig_DEVELOPMENT from './config/config.development';
       autoLoadModels: true,
       models: [User, Otp],
       synchronize: process.env.DB_SYNCHRONIZE == "true" ? true : false,
+      dialectOptions: {
+        alert: process.env.NODE_ENV == "development" ? true : false,
+      },
+      sync: {
+        force: process.env.NODE_ENV == "development" ? true : false,
+      },
       retryDelay: 3000,
+
     }),
 
     UserModule],
